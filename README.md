5d6135b9829839b482d5059e36d2f6b3e189e186
Donovan Said <donovan.s.said@gmail.com>
2024-02-14 12:32:46 +0000

    Removed duplicate code.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tutorial-learning-go
 
-I have a fragmetted knowledge of Golang, so I'ver deceided to start from scratch.
+I have a fragmetted knowledge of Golang, so I've deceided to start from scratch.
 
 This repository is used to follow the [W3 Schools Go Tutorial](https://www.w3schools.com/go/index.php)
--- tutorials/data_types.go
+++ tutorials/data_types.go
@@ -5,16 +5,5 @@ import (
 )
 
 func data_types_tutorial() {
-	// Camel Case
-	myVariableName := "John"
-
-	// Pascal Case
-	MyVariableName := "John"
-
-	// Snake Case
-	my_variable_name := "John"
-
-	fmt.Println(myVariableName)
-	fmt.Println(MyVariableName)
-	fmt.Println(my_variable_name)
+	fmt.Print("Placeholder")
 }
