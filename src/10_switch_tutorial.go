d6ad09dc8453948f1b7a3bbf6d68870d69b26349
Donovan Said <donovan.s.said@gmail.com>
2024-02-15 12:10:08 +0000

    Finished switch tutorial.

--- src/10_switch_tutorial.go
+++ src/10_switch_tutorial.go
@@ -10,5 +10,56 @@ import (
 )
 
 func switch_tutorial() {
-	fmt.Print("PLACEHOLDER")
+	/* Use the switch statement to select one of many code blocks to be
+	executed.
+
+	The switch statement in Go is similar to the ones in C, C++, Java,
+	JavaScript, and PHP. The difference is that it only runs the matched case so
+	it does not need a break statement
+
+	This is how it works:
+
+	- The expression is evaluated once
+	- The value of the switch expression is compared with the values of each
+	  case
+	- If there is a match, the associated block of code is executed
+	- The default keyword is optional. It specifies some code to run if there is
+	  no case match
+	*/
+
+	day := 4
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	}
+
+	/* It is possible to have multiple values for each case in the switch
+	statement
+
+	The example below uses the weekday number to return different text
+	*/
+
+	switch day {
+	case 1, 3, 5:
+		fmt.Println("Odd weekday")
+	case 2, 4:
+		fmt.Println("Even weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid day of day number")
+	}
 }
--- src/8_operators_tutorial.go
+++ src/8_operators_tutorial.go
@@ -14,5 +14,45 @@ import (
 )
 
 func operators_tutorial() {
-	fmt.Print("PLACEHOLDER")
+
+	var a = 15 + 25
+	fmt.Println(a)
+
+	/* Although the + operator is often used to add together two values, it can also be used to add together a variable
+	and a value, or a variable and another variable */
+
+	var (
+		sum1 = 100 + 50    // 150 (100 + 50)
+		sum2 = sum1 + 250  // 400 (150 + 250)
+		sum3 = sum2 + sum2 // 800 (400 + 400)
+	)
+	fmt.Println(sum3)
+
+	/* Assignment Operators
+
+	Assignment operators are used to assign values to variables.
+
+	In the example below, we use the assignment operator (=) to assign the value
+	10 to a variable called x
+	*/
+
+	var x = 10
+	fmt.Println(x)
+
+	// The addition assignment operator (+=) adds a value to a variable
+	x += 5
+	fmt.Println(x)
+
+	/* Comparison Operators
+
+	Comparison operators are used to compare two values.
+
+	Note: The return value of a comparison is either true (1) or false (0).
+
+	In the following example, we use the greater than operator (>) to find out if 5 is greater than 3
+	*/
+
+	var c = 5
+	var d = 3
+	fmt.Println(c > d) // returns 1 (true) because 5 is greater than 3
 }
